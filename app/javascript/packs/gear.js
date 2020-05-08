bf49ec7edbb796701507915de2fc0d243e45eb35
INexizI <39536903+INexizI@users.noreply.github.com>
2020-05-08 18:08:32 +0300

    add gear star

--- app/javascript/packs/gear.js
+++ app/javascript/packs/gear.js
@@ -7,5 +7,42 @@
       }
       $(this).next('.g-main').fadeToggle('fast').css(cssVal);
     });
+    // button toggle
+    $('.bt0').click(function() {
+      $('#bt0').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt0').hide('fast');
+    });
+    $('.bt1').click(function() {
+      $('#bt1').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt1').hide('fast');
+    });
+    $('.bt2').click(function() {
+      $('#bt2').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt2').hide('fast');
+    });
+    $('.bt3').click(function() {
+      $('#bt3').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt3').hide('fast');
+    });
+    $('.bt4').click(function() {
+      $('#bt4').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt4').hide('fast');
+    });
+    $('.bt5').click(function() {
+      $('#bt5').toggle('fast');
+      $(this).toggleClass('b-act');
+      $('button').not(this).removeClass('b-act');
+      $('.g-stat p').not('#bt5').hide('fast');
+    });
   });
 }).call(this);
