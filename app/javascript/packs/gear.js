d33deeb430221542c7701d8330d851c42f986b22
INexizI <39536903+INexizI@users.noreply.github.com>
2020-05-15 16:01:51 +0300

    gear script upd

--- app/javascript/packs/gear.js
+++ app/javascript/packs/gear.js
@@ -1,46 +1,21 @@
 (function() {
   $(document).on("turbolinks:load", function() {
     $('.g-title').click(function() {
-      var cssVal = {
+      $cssVal = {
         'display':'flex',
         'flex-wrap':'wrap'
       }
-      $(this).next('.g-main').fadeToggle('fast').css(cssVal);
+      $(this).next('.g-main').fadeToggle('fast').css($cssVal);
     });
-    $('.bt1').click(function() {
-      $(this).next('#bt1').show();
-      $('.btn').not('#bt1').hide();
-      $(this).addClass('b-act');
-      $(this).prevAll('button').addClass('b-act');
-      $(this).nextAll('button').removeClass('b-act');
-    });
-    $('.bt2').click(function() {
-      $(this).next('#bt2').show();
-      $('.btn').not('#bt2').hide();
-      $(this).addClass('b-act');
-      $(this).prevAll('button').addClass('b-act');
-      $(this).nextAll('button').removeClass('b-act');
-    });
-    $('.bt3').click(function() {
-      $(this).next('#bt3').show();
-      $('.btn').not('#bt3').hide();
-      $(this).addClass('b-act');
-      $(this).prevAll('button').addClass('b-act');
-      $(this).nextAll('button').removeClass('b-act');
-    });
-    $('.bt4').click(function() {
-      $(this).next('#bt4').show();
-      $('.btn').not('#bt4').hide();
-      $(this).addClass('b-act');
-      $(this).prevAll('button').addClass('b-act');
-      $(this).nextAll('button').removeClass('b-act');
-    });
-    $('.bt5').click(function() {
-      $(this).next('#bt5').show();
-      $('.btn').not('#bt5').hide();
-      $(this).addClass('b-act');
-      $(this).prevAll('button').addClass('b-act');
-      $(this).nextAll('button').removeClass('b-act');
+    $('button').click(function() {
+      $btn = $(this).attr('class');
+      $btnVal = $(this).parent().next('.g-val').find('#' + $btn);
+      $($btnVal).show();
+      $('.btn').not($btnVal).hide();
+      $('button').css('background', '#f0f0f0');
+      $(this).css('background', '#808080');
+      $(this).prevAll('button').css('background', '#808080');
+      $(this).nextAll('button').css('background', '#f0f0f0');
     });
   });
 }).call(this);
