c5f61b69f7408fd6432f185419ddff5102eccc86
INexizI <39536903+INexizI@users.noreply.github.com>
2020-05-21 15:10:16 +0300

    calc select upd

--- app/javascript/packs/calc.js
+++ app/javascript/packs/calc.js
@@ -1,11 +1,31 @@
 (function() {
   $(document).on("turbolinks:load", function() {
-    $('#char_name').change(function () {
-      var str = '';
-      $('#char_name option:selected').each(function() {
-        str += $(this).text() + ' ';
-      });
-      $('.calc-stat').text(str);
+    $('select#char_name').change(function () {
+      var selectedHero = $(this).children('option:selected').val();
+      $('.char-id').text(selectedHero);
     }).change();
+    $('select#role_name').change(function () {
+      var selectedRole = $(this).children('option:selected').val();
+      $('.role-id').text(selectedRole);
+    }).change();
+
+    (function() {
+      var states;
+      $('#calc_char_id').parent().hide();
+      states = $('#calc_char_id').html();
+      $('#calc_role_id').change(function() {
+        var country, escaped_country, options;
+        country = $('#calc_role_id :selected').text();
+        escaped_country = country.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = $(states).filter("optgroup[label='" + escaped_country + "']").html();
+        if (options) {
+          $('#calc_char_id').html(options);
+          return $('#calc_char_id').parent().show();
+        } else {
+          $('#calc_char_id').empty();
+          return $('#calc_char_id').parent().hide();
+        }
+      });
+    }).call(this);
   });
 }).call(this);
--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,6 @@ class Role < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: %i[slugged finders]
 
+  has_many :chars
   has_many :stats, class_name: 'Stat'
 end
