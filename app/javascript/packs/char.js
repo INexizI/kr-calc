6dfebc2253d7602c8c0fdf0ae6a8e2d65f0210c2
INexizI <39536903+INexizI@users.noreply.github.com>
2020-06-25 01:47:12 +0300

    rework hero images upload

--- app/javascript/packs/char.js
+++ app/javascript/packs/char.js
@@ -7,5 +7,24 @@
       }
       $(this).next('.h-main').fadeToggle('fast').css(cssVal);
     });
+    $('.c-main #c-class').each(function() {
+      $heroClass = $(this).text();
+      $heroName = $heroClass.toUpperCase();
+      $('#c-class').empty().append("<img src='/images/media/classes/" + $heroName + ".png' title='" + $heroClass + "'>");
+    });
+    $('.c-main .c-avatar').each(function() {
+      $heroAvatar = $('#c-name').text();
+      $(this).append("<img src='/images/media/heroes/" + $heroAvatar + "/portrait.png'>");
+    });
+    $('.c-sk #sk-img').each(function(index, el) {
+      $heroSkills = $('#c-name').text();
+      $s = $(el).attr('class');
+      $('.' + $s).append("<img src='/images/media/heroes/" + $heroSkills + "/" + $s + ".png'>");
+    });
+    $('.p-avatar').children().each(function(index, el) {
+      $char = $('.p-hero').text();
+      $s = $(el).attr('class');
+      $('.' + $s).append("<img src='/images/media/heroes/" + $char + "/" + $s + ".png'>");
+    });
   });
 }).call(this);
