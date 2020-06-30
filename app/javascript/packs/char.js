c17bfe6524b9a027ade8a524bacfca042d34fd71
INexizI <39536903+INexizI@users.noreply.github.com>
2020-07-01 01:09:51 +0300

    cahr show fix

--- app/javascript/packs/char.js
+++ app/javascript/packs/char.js
@@ -26,5 +26,23 @@
       $s = $(el).attr('class');
       $('.' + $s).append("<img src='/images/media/heroes/" + $char + "/" + $s + ".png'>");
     });
+    $('.c-perk-img').mouseenter(function() {
+      $left = 0;
+      $top = 0;
+      if ($(document).width() - $(this).offset().left < $(document).width() / 2)
+        $left = -1;
+      // if ($(document).height() - $(this).offset().top < $(document).height() / 2)
+      //   $top = -1;
+      $y = 10;
+      $x = $left * 10 + $left * 400;
+      $cs = {
+        'top':$y,
+        'left': $x,
+        'display': 'grid'
+      }
+      $('.s-description', this).css($cs);
+    }).mouseleave(function() {
+      $('.s-description').hide();
+    });
   });
 }).call(this);
