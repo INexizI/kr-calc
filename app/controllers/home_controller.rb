ef27fb95367e3c49d5788cb124e6bd70bb1fd744
INexizI <39536903+INexizI@users.noreply.github.com>
2020-07-10 17:11:29 +0300

    home page re-work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    @chars = Char.all
+    @char = Char.offset(rand(Char.count)).first
+    @gears = Gear.all
+    @gear = Gear.offset(rand(Gear.count)).first
+    @perks = Perk.all
+    @perk = Perk.offset(rand(Perk.count)).first
+    @roles = Role.all
+    @role = Role.offset(rand(Role.count)).first
+    @runes = Rune.all
+    @rune = Rune.offset(rand(Rune.count)).first
+    @skills = Skill.all
+    @skill = Skill.offset(rand(Skill.count)).first
+    @stats = Stat.all
+    @stat = Stat.offset(rand(Stat.count)).first
   end
 end
