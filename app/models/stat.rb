28dd1df06f5ae3198c8d139f650dd90837924175
INexizI <39536903+INexizI@users.noreply.github.com>
2020-07-27 19:17:46 +0300

    gear desc upd

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -3,6 +3,10 @@ class Stat < ApplicationRecord
   # belongs_to :gear, optional: true
   has_many :gears
 
+  def star_0
+    self.value
+  end
+
   def star_1
     @x = self.value.to_f
     self.value = (@x + @x / 100 * 10).ceil
