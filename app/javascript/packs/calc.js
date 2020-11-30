aa8293297e453ca624a5d63532a6ba6d4ef57601
INexizI <39536903+INexizI@users.noreply.github.com>
2020-11-30 15:28:45 -0800

    add all gear option stat on calc

--- app/javascript/packs/calc.js
+++ app/javascript/packs/calc.js
@@ -327,6 +327,7 @@
     }).change();
 
     $('.ay').change(function() {
+      $stN = $(this).parent().prev().children().val();
       statQ();
     });
 
@@ -574,7 +575,8 @@
       });
     }
     function statValue() {
-      $ax = '<option value="">- - - - - - - - - -</option><option value="ATK">ATK</option><option value="ATK Spd">ATK Spd</option><option value="Crit">Crit</option><option value="Crit DMG">Crit DMG</option><option value="MP Recovery/Attack">MP Recovery/Attack</option><option value="MP Recovery/Sec">MP Recovery/Sec</option><option value="Penetration">Penetration</option><option value="Lifesteal">Lifesteal</option><option value="ACC">ACC</option><option value="Debuf ACC">Debuf ACC</option><option value="Max HP">Max HP</option><option value="CC Resist">CC Resist</option><option value="Block">Block</option><option value="P.Block">P.Block</option><option value="M.Block">M.Block</option><option value="Crit Resistance">Crit Resistance</option><option value="P.Crit Resistance">P.Crit Resistance</option><option value="M.Crit Resistance">M.Crit Resistance</option>'
+      $ax = '<option value="">- - - - - - - - - -</option><option value="ATK">ATK</option><option value="ATK Spd">ATK Spd</option><option value="Crit">Crit</option><option value="Crit DMG">Crit DMG</option><option value="MP Recovery/Attack">MP Recovery/Attack</option><option value="MP Recovery/Sec">MP Recovery/Sec</option><option value="Penetration">Penetration</option><option value="Lifesteal">Lifesteal</option><option value="ACC">ACC</option><option value="Debuf ACC">Debuf ACC</option><option value="Max HP">Max HP</option><option value="CC Resist">CC Resist</option><option value="Block">Block</option><option value="P.Block">P.Block</option><option value="M.Block">M.Block</option><option value="Crit Resistance">Crit Resistance</option><option value="P.Crit Resistance">P.Crit Resistance</option><option value="M.Crit Resistance">M.Crit Resistance</option><option value="DEF">DEF</option><option value="P.DEF">P.DEF</option><option value="M.DEF">M.DEF</option><option value="Dodge">Dodge</option><option value="P.Dodge">P.Dodge</option><option value="M.Dodge">M.Dodge</option>'
+      $aTr = '<option value="Tough">Tough</option><option value="P.Tough">P.Tough</option><option value="M.Tough">M.Tough</option><option value="Recovery">Recovery</option><option value="Mana Recovery upon taking DMG">Mana Recovery upon taking DMG</option><option value="DMG Reduction upon Block">DMG Reduction upon Block</option><option value="DMG Reduction upon P.Block">DMG Reduction upon P.Block</option><option value="DMG Reduction upon M.Block">DMG Reduction upon M.Block</option>'
       $ay = '<optgroup id="q"><option value=""></option></optgroup><optgroup id="q1" label="Stat"><option value="7">7</option><option value="7.5">7.5</option><option value="8">8</option><option value="8.5">8.5</option><option value="9">9</option><option value="9.5">9.5</option><option value="10">10</option><option value="10.5">10.5</option><option value="11">11</option><option value="11.5">11.5</option><option value="12">12</option></optgroup><optgroup id="q2" label="Stat"><option value="21">21</option><option value="22.5">22.5</option><option value="24">24</option><option value="25.5">25.5</option><option value="27">27</option><option value="28.5">28.5</option><option value="30">30</option><option value="31.5">31.5</option><option value="33">33</option><option value="34.5">34.5</option><option value="36">36</option></optgroup><optgroup id="q3" label="Stat"><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option></optgroup><optgroup id="q4" label="Stat"><option value="70">70</option><option value="75">75</option><option value="80">80</option><option value="85">85</option><option value="90">90</option><option value="95">95</option><option value="100">100</option><option value="105">105</option><option value="110">110</option><option value="115">115</option><option value="120">120</option></optgroup><optgroup id="q5" label="Stat"><option value="140">140</option><option value="150">150</option><option value="160">160</option><option value="170">170</option><option value="180">180</option><option value="190">190</option><option value="200">200</option><option value="210">210</option><option value="220">220</option><option value="230">230</option><option value="240">240</option></optgroup><optgroup id="q6" label="Stat"><option value="60">60</option><option value="65">65</option><option value="70">70</option><option value="75">75</option><option value="80">80</option><option value="85">85</option><option value="90">90</option><option value="95">95</option><option value="100">100</option><option value="105">105</option><option value="110">110</option></optgroup><optgroup id="q7" label="Stat"><option value="35">35</option><option value="37">37</option><option value="40">40</option><option value="42">42</option><option value="45">45</option><option value="47">47</option><option value="50">50</option><option value="52">52</option><option value="55">55</option><option value="57">57</option><option value="60">60</option></optgroup>'
       $('.opt').find('.ax').each(function() {
         $(this).html($ax);
@@ -582,23 +584,21 @@
       $('.opt').find('.ay').each(function() {
         $(this).html($ay);
       });
+      $('.opt').find('#calc_st_treasure').each(function() {
+        $(this).html($ax + $aTr);
+      });
     }
     function statQ() {
       $c = 0;
-      $sumAtk = 0;$sumSpd = 0;
+      $sum = 0;
       $('#atk-opt, #spd-opt').empty();
       $x = $('.opt .ax').serializeArray()
       $y = $('.opt .ay').serializeArray();
       $.each($x, function(iX, n) {
-        if (n.value == 'ATK') {
+        if (n.value == $stN) {
           $c = $('.opt .ay').serializeArray()[iX];
-          $sumAtk += Number(parseFloat($c.value));
-          $('#atk-opt').text($sumAtk);
-        }
-        else if (n.value == 'ATK Spd') {
-          $c = $('.opt .ay').serializeArray()[iX];
-          $sumSpd += Number(parseFloat($c.value));
-          $('#spd-opt').text($sumSpd);
+          $sum += Number(parseFloat($c.value));
+          $('.totalStat').find('p[name="' + $stN + '"]').text($sum);
         }
       });
     }
