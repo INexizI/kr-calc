a1b564abada4b288b62a7dbc393faa1ee9c32a23
INexizI <39536903+INexizI@users.noreply.github.com>
2020-12-16 19:36:02 -0800

    fix Treasure stats
    Block DEF and Toughness

--- app/javascript/packs/calc.js
+++ app/javascript/packs/calc.js
@@ -575,19 +575,19 @@
           $(this).parent().next().children().find('#q2').show();
         else if (($st == 'Crit DMG') || ($st == 'P.DEF') || ($st == 'M.DEF') || ($st == 'Recovery'))
           $(this).parent().next().children().find('#q3').show();
-        else if (($st == 'ATK Spd') || ($st == 'Crit') || ($st == 'Lifesteal') || ($st == 'ACC') || ($st == 'Debuff ACC') || ($st == 'CC Resist') || ($st == 'Block') || ($st == 'Crit Resistance') || ($st == 'P.Dodge') || ($st == 'M.Dodge') || ($st == 'P.Tough') || ($st == 'M.Tough') || ($st == 'DMG Reduction upon P.Block') || ($st == 'DMG Reduction upon M.Block'))
+        else if (($st == 'ATK Spd') || ($st == 'Crit') || ($st == 'Lifesteal') || ($st == 'ACC') || ($st == 'Debuff ACC') || ($st == 'CC Resist') || ($st == 'Block') || ($st == 'Crit Resistance') || ($st == 'P.Dodge') || ($st == 'M.Dodge') || ($st == 'P.Tough') || ($st == 'M.Tough') || ($st == 'P.Resistance') || ($st == 'M.Resistance') || ($st == 'DMG Reduction upon P.Block') || ($st == 'DMG Reduction upon M.Block') || ($st == 'P.Block DEF') || ($st == 'M.Block DEF'))
           $(this).parent().next().children().find('#q4').show();
         else if (($st == 'MP Recovery/Attack') || ($st == 'P.Block') || ($st == 'M.Block') || ($st == 'P.Crit Resistance') || ($st == 'M.Crit Resistance'))
           $(this).parent().next().children().find('#q5').show();
         else if ($st == 'Penetration')
           $(this).parent().next().children().find('#q6').show();
-        else if (($st == 'Dodge') || ($st == 'Tough') || ($st == 'DMG Reduction upon Block'))
+        else if (($st == 'Dodge') || ($st == 'Tough') || ($st == 'Resistance') || ($st == 'DMG Reduction upon Block'))
           $(this).parent().next().children().find('#q7').show();
       });
     }
     function statValue() {
       $ax = '<option value="">- - - - - - - - - -</option><option value="ATK">ATK</option><option value="ATK Spd">ATK Spd</option><option value="Crit">Crit</option><option value="Crit DMG">Crit DMG</option><option value="MP Recovery/Attack">MP Recovery/Attack</option><option value="MP Recovery/Sec">MP Recovery/Sec</option><option value="Penetration">Penetration</option><option value="Lifesteal">Lifesteal</option><option value="ACC">ACC</option><option value="Debuff ACC">Debuff ACC</option><option value="Max HP">Max HP</option><option value="CC Resist">CC Resist</option><option value="Block">Block</option><option value="P.Block">P.Block</option><option value="M.Block">M.Block</option><option value="Crit Resistance">Crit Resistance</option><option value="P.Crit Resistance">P.Crit Resistance</option><option value="M.Crit Resistance">M.Crit Resistance</option><option value="DEF">DEF</option><option value="P.DEF">P.DEF</option><option value="M.DEF">M.DEF</option><option value="Dodge">Dodge</option><option value="P.Dodge">P.Dodge</option><option value="M.Dodge">M.Dodge</option>'
-      $axTr = '<option value="Tough">Tough</option><option value="P.Tough">P.Tough</option><option value="M.Tough">M.Tough</option><option value="Recovery">Recovery</option><option value="Mana Recovery upon taking DMG">Mana Recovery upon taking DMG</option><option value="DMG Reduction upon Block">DMG Reduction upon Block</option><option value="DMG Reduction upon P.Block">DMG Reduction upon P.Block</option><option value="DMG Reduction upon M.Block">DMG Reduction upon M.Block</option>'
+      $axTr = '<option value="Resistance">Resistance</option><option value="P.Resistance">P.Resistance</option><option value="M.Resistance">M.Resistance</option><option value="Recovery">Recovery</option><option value="Mana Recovery upon taking DMG">Mana Recovery upon taking DMG</option><option value="DMG Reduction upon Block">DMG Reduction upon Block</option><option value="DMG Reduction upon P.Block">DMG Reduction upon P.Block</option><option value="DMG Reduction upon M.Block">DMG Reduction upon M.Block</option>'
       $ay = '<optgroup id="q"><option value="0">- - - </option></optgroup><optgroup id="q1" label="Stat"><option value="7">7</option><option value="7,5">7,5</option><option value="8">8</option><option value="8,5">8,5</option><option value="9">9</option><option value="9,5">9,5</option><option value="10">10</option><option value="10,5">10,5</option><option value="11">11</option><option value="11,5">11,5</option><option value="12">12</option></optgroup><optgroup id="q2" label="Stat"><option value="21">21</option><option value="22,5">22,5</option><option value="24">24</option><option value="25,5">25,5</option><option value="27">27</option><option value="28,5">28,5</option><option value="30">30</option><option value="31,5">31,5</option><option value="33">33</option><option value="34,5">34,5</option><option value="36">36</option></optgroup><optgroup id="q3" label="Stat"><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option></optgroup><optgroup id="q4" label="Stat"><option value="70">70</option><option value="75">75</option><option value="80">80</option><option value="85">85</option><option value="90">90</option><option value="95">95</option><option value="100">100</option><option value="105">105</option><option value="110">110</option><option value="115">115</option><option value="120">120</option></optgroup><optgroup id="q5" label="Stat"><option value="140">140</option><option value="150">150</option><option value="160">160</option><option value="170">170</option><option value="180">180</option><option value="190">190</option><option value="200">200</option><option value="210">210</option><option value="220">220</option><option value="230">230</option><option value="240">240</option></optgroup><optgroup id="q6" label="Stat"><option value="60">60</option><option value="65">65</option><option value="70">70</option><option value="75">75</option><option value="80">80</option><option value="85">85</option><option value="90">90</option><option value="95">95</option><option value="100">100</option><option value="105">105</option><option value="110">110</option></optgroup><optgroup id="q7" label="Stat"><option value="35">35</option><option value="37">37</option><option value="40">40</option><option value="42">42</option><option value="45">45</option><option value="47">47</option><option value="50">50</option><option value="52">52</option><option value="55">55</option><option value="57">57</option><option value="60">60</option></optgroup>'
       $ayTr = '<optgroup id="q"><option value="0">- - - </option></optgroup><optgroup id="q1" label="Stat"><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></optgroup><optgroup id="q2" label="Stat"><option value="15">15</option><option value="18">18</option><option value="21">21</option><option value="24">24</option><option value="27">27</option></optgroup><optgroup id="q3" label="Stat"><option value="10">10</option><option value="12">12</option><option value="14">14</option><option value="16">16</option><option value="18">18</option></optgroup><optgroup id="q4" label="Stat"><option value="50">50</option><option value="60">60</option><option value="70">70</option><option value="80">80</option><option value="90">90</option></optgroup><optgroup id="q5" label="Stat"><option value="100">100</option><option value="120">120</option><option value="140">140</option><option value="160">160</option><option value="180">180</option></optgroup><optgroup id="q6" label="Stat"><option value="50">50</option><option value="60">60</option><option value="70">70</option><option value="80">80</option><option value="90">90</option></optgroup><optgroup id="q7" label="Stat"><option value="25">25</option><option value="30">30</option><option value="35">35</option><option value="40">40</option><option value="45">45</option></optgroup>'
       $('.opt').find('.ax').each(function() {
@@ -868,12 +868,6 @@
       $statManaRec = $('.class-stats .role' + $gearClass).find('p').filter(function() {
         return $(this).text() === 'Mana Recovery upon taking DMG'
       }).next('p');
-      $statPRed = $('.class-stats .role' + $gearClass).find('p').filter(function() {
-        return $(this).text() === 'DMG Reduction upon P.Block'
-      }).next('p');
-      $statMRed = $('.class-stats .role' + $gearClass).find('p').filter(function() {
-        return $(this).text() === 'DMG Reduction upon M.Block'
-      }).next('p');
 
       $f = 0;$fr = 0;$p = 0;$d = 0;$la = 0;$le = 0;$ch = 0;$s = 0;$pr = 0;$dl = 0;$t = 0;
       $sq = $('.set').find('p').each(function() {
@@ -975,12 +969,6 @@
       }).next('p');
       $tManaRec = $('.t-total .r-stats').find('p').filter(function() {
         return $(this).text() === 'Mana Recovery upon taking DMG'
-      }).next('p');
-      $tDRedP = $('.t-total .r-stats').find('p').filter(function() {
-        return $(this).text() === 'DMG Reduction upon P.Block'
-      }).next('p');
-      $tDRedM = $('.t-total .r-stats').find('p').filter(function() {
-        return $(this).text() === 'DMG Reduction upon M.Block'
       }).next('p');
 
       $stCrit = $('p[name="Crit"]').text();
@@ -1426,6 +1414,15 @@
         } else if (n.value == 'M.Tough') {
           $sMTgh += Number(parseFloat($y.value));
           $('.totalStat').find('p[name="M.Tough"]').text($sMTgh);
+        } else if (n.value == 'Resistance') {
+          $sTgh += Number(parseFloat($y.value));
+          $('.totalStat').find('p[name="Tough"]').text($sTgh);
+        } else if (n.value == 'P.Resistance') {
+          $sPTgh += Number(parseFloat($y.value));
+          $('.totalStat').find('p[name="P.Tough"]').text($sPTgh);
+        } else if (n.value == 'M.Resistance') {
+          $sMTgh += Number(parseFloat($y.value));
+          $('.totalStat').find('p[name="M.Tough"]').text($sMTgh);
         } else if (n.value == 'Recovery') {
           $sRec += Number(parseFloat($y.value));
           $('.totalStat').find('p[name="Recovery"]').text($sRec);
@@ -1434,13 +1431,13 @@
           $('.totalStat').find('p[name="Mana Recovery upon taking DMG"]').text($sMRec);
         } else if (n.value == 'DMG Reduction upon Block') {
           $sDRB += Number(parseFloat($y.value));
-          $('.totalStat').find('p[name="DMG Reduction upon Block"]').text($sDRB);
+          $('.totalStat').find('p[name="Block DEF"]').text($sDRB);
         } else if (n.value == 'DMG Reduction upon P.Block') {
           $sDRPB += Number(parseFloat($y.value));
-          $('.totalStat').find('p[name="DMG Reduction upon P.Block"]').text($sDRPB);
+          $('.totalStat').find('p[name="P.Block DEF"]').text($sDRPB);
         } else if (n.value == 'DMG Reduction upon M.Block') {
           $sDRMB += Number(parseFloat($y.value));
-          $('.totalStat').find('p[name="DMG Reduction upon M.Block"]').text($sDRMB);
+          $('.totalStat').find('p[name="M.Block DEF"]').text($sDRMB);
         }
       });
     }
