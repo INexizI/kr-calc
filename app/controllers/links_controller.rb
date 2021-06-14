99743477aff8b96fbe661828826e771ab63e8345
INexizI <39536903+INexizI@users.noreply.github.com>
2021-06-14 12:14:13 +0300

    upd link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,7 @@ class LinksController < ApplicationController
     resp = Net::HTTP.get_response(URI.parse(source))
     data = JSON.parse(resp.body)
     @links = data
-    @q = request.original_url.byteslice(28, 30)
+    # @q = request.original_url.byteslice(28, 30) #localhost
+    @q = request.original_url.byteslice(36, 38) #heroku
   end
 end
