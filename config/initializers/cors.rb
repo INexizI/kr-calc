80b69e9c04c8dc78cb9c6bde0997975afd1912d3
INexizI <39536903+INexizI@users.noreply.github.com>
2021-06-16 23:44:47 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://krsharelink.herokuapp.com/', 'https://kr-calc.herokuapp.com/', 'http://localhost:3000/'
+    origins 'https://krsharelink.herokuapp.com/', 'https://kr-calc.herokuapp.com/', 'http://localhost:3000/', 'https://www.kr-calc.com'
     resource '*', headers: :any, methods: [:get, :post]
   end
 end
