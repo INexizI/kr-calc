3cf75b34e8b348f7500e1d4f137f86b2ef25810d
INexizI <39536903+INexizI@users.noreply.github.com>
2021-06-26 15:50:14 +0300

    fix link migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_155436) do
+ActiveRecord::Schema.define(version: 2021_06_26_124752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
